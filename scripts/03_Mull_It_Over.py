7495984c36d8e3c49b567411a3a5fa3b57836f9b
Kathryn Kleijn <137423893+kathrynpotten@users.noreply.github.com>
2024-12-03 11:49:14 +0000

    3-2 Mull It Over

--- scripts/03_Mull_It_Over.py
+++ scripts/03_Mull_It_Over.py
@@ -48,3 +48,37 @@ with open("../input_data/03_Mull_It_Over.txt", "r", encoding="utf-8") as file:
 instructions = find_real_instructions(input)
 answer_1 = decode_instructions(instructions)
 print(answer_1)
+
+
+# Part 2
+
+test_data2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+test_answer2 = 48
+
+
+def split_on_conditionals(input):
+    do_sections = []
+    sections = input.split("do()")
+    for section in sections:
+        dont_sections = section.split("don't()")
+        do_sections.append(dont_sections[0])
+    return do_sections
+
+
+assert split_on_conditionals(test_data2) == ["xmul(2,4)&mul[3,7]!^", "?mul(8,5))"]
+
+
+def decode_with_conditionals(input):
+    do_sections = split_on_conditionals(input)
+    mul = 0
+    for section in do_sections:
+        instructions = find_real_instructions(section)
+        mul += decode_instructions(instructions)
+    return mul
+
+
+assert decode_with_conditionals(test_data2) == test_answer2
+
+
+answer_2 = decode_with_conditionals(input)
+print(answer_2)
