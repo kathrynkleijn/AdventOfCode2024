e8a7bdebc47ee30f9fdb0310f076b817424f0d3d
Kathryn Kleijn <137423893+kathrynpotten@users.noreply.github.com>
2024-12-05 14:28:49 +0000

    5-1 Print Queue

--- scripts/05_Print_Queue.py
+++ scripts/05_Print_Queue.py
@@ -51,13 +51,51 @@ def create_rule_list(rules):
             first_index = rule_list.index(first)
             second_index = rule_list.index(second)
             if first_index > second_index:
+                move = rule_list[second_index:first_index]
+                rule_list = rule_list[:second_index] + rule_list[first_index:]
+                rule_list.extend(move)
+        # print(rule, rule_list)
+    return rule_list
+
+
+def create_rule_list_old(rules):
+    rule_list = rules[0].split("|")
+    for rule in rules:
+        first, second = rule.split("|")
+        if first not in rule_list and second not in rule_list:
+            rule_list.extend([first, second])
+        elif first in rule_list and second not in rule_list:
+            rule_list.append(second)
+        elif first not in rule_list and second in rule_list:
+            rule_list = rule_list[::-1]
+            rule_list.append(first)
+            rule_list = rule_list[::-1]
+        else:
+            first_index = rule_list.index(first)
+            second_index = rule_list.index(second)
+            if first_index > second_index:
                 rule_list.insert(second_index, rule_list.pop(first_index))
     return rule_list
 
 
-# assert create_rule_list([test_rules.split("\n")[0]]) == ["47", "53"]
+assert create_rule_list([test_rules.split("\n")[0]]) == ["47", "53"]
 test_rule_list = create_rule_list(test_rules.split("\n"))
+test_rule_list_old = create_rule_list_old(test_rules.split("\n"))
 print(test_rule_list)
+print(test_rule_list_old)
+
+
+def check_correct(rule_list, rules):
+    for rule in rules:
+        first, second = rule.split("|")
+        first_index = rule_list.index(first)
+        second_index = rule_list.index(second)
+        if first_index > second_index:
+            return False, rule
+    return True, ""
+
+
+assert check_correct(test_rule_list, test_rules.split("\n"))[0] == True
 
 
 def check_order(line, rule_list):
@@ -66,7 +104,7 @@ def check_order(line, rule_list):
     return line == check_list
 
 
-# assert check_order("75,47,61,53,29", test_rule_list) == True
+assert check_order("75,47,61,53,29", test_rule_list) == True
 
 
 def sum_of_middle(rule_list, lines):
@@ -77,14 +115,19 @@ def sum_of_middle(rule_list, lines):
     return sum
 
 
-# assert sum_of_middle(test_rule_list, test_lines.split("\n")) == test_answer
+assert sum_of_middle(test_rule_list, test_lines.split("\n")) == test_answer
 
 
 with open("../input_data/05_Print_Queue.txt", "r", encoding="utf-8") as file:
     input_rules, input_lines = file.read().strip().split("\n\n")
 
 
-input_rule_list = create_rule_list(input_rules.split("\n"))
-print(input_rule_list)
-answer1 = sum_of_middle(input_rule_list, input_lines.split("\n"))
-print(answer1)
+# test_list_2 = input_rules.split("\n")[:29]
+# rule_list = create_rule_list(test_list_2)
+# print(check_correct(rule_list, test_list_2))
+# input_rule_list = create_rule_list(input_rules.split("\n"))
+# print(input_rule_list)
+
+# print(check_correct(input_rule_list, input_rules.split("\n")))
+# answer1 = sum_of_middle(input_rule_list, input_lines.split("\n"))
+# print(answer1)
