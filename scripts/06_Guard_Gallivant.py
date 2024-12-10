f2f00b1dff3994ef74e9ee8b9cc5355d353ff023
Kathryn Kleijn <137423893+kathrynpotten@users.noreply.github.com>
2024-12-10 10:33:37 +0000

    6-2 Guard Gallivant

--- scripts/06_Guard_Gallivant.py
+++ scripts/06_Guard_Gallivant.py
@@ -22,13 +22,14 @@ class LabMap:
     def __init__(self, map):
         self.map = map
         self.lines = map.split("\n")
-        self.debug_map = map.split("\n")
-        self.m = len(self.lines[0])
-        self.n = len(self.lines)
-        self.size = (self.n, self.m)
+        # self.debug_map = map.split("\n")
+        self.columns = self.to_columns()
+        self.m = len(self.lines)
+        self.n = len(self.columns)
+        self.size = (self.m, self.n)
         self.start = self.start_point()
         self.visited = {}
-        self.direction = self.lines[self.start[0]][self.start[1]]
+        self.start_direction = self.lines[self.start[0]][self.start[1]]
 
     def __str__(self):
         return "\n".join(line for line in self.lines)
@@ -37,6 +38,12 @@ class LabMap:
         row_num = math.floor(self.map.find("^") / (self.m + 3)) + 1
         column_num = self.lines[row_num].find("^")
         return (row_num, column_num)
+
+    def to_columns(self):
+        self.columns = []
+        for col in range(len(self.lines[0])):
+            self.columns.append("".join(row[col] for row in self.lines))
+        return self.columns
 
     def check_up(self, current_row, current_col):
         return self.lines[current_row - 1][current_col] in ["#", "O"]
@@ -69,50 +76,112 @@ class LabMap:
             )
             self.visited[(current_row, current_col)].append(direction)
 
+    def find_next_obstruction(self, current_row, current_col, direction):
+        new_position = (current_row, current_col)
+        if direction == "^":
+            try:
+                next_obstruction = self.columns[current_col][:current_row].find("#")
+            except:
+                next_obstruction = -1
+            if next_obstruction < new_position[0] - 1 and next_obstruction >= 0:
+                new_position = (next_obstruction + 1, current_col)
+                for row in range(new_position[0], current_row + 1):
+                    self.lines[row] = (
+                        self.lines[row][:current_col]
+                        + "X"
+                        + self.lines[row][current_col + 1 :]
+                    )
+            else:
+                new_position = (-1, -1)
+                for row in range(current_row + 1):
+                    self.lines[row] = (
+                        self.lines[row][:current_col]
+                        + "X"
+                        + self.lines[row][current_col + 1 :]
+                    )
+            direction = ">"
+
+        elif direction == ">":
+            try:
+                next_obstruction = (
+                    self.lines[current_row][current_col:].find("#") + current_col
+                )
+            except:
+                next_obstruction = -1
+            if next_obstruction > new_position[1] + 1:
+                new_position = (current_row, next_obstruction - 1)
+                num_moves = new_position[1] - current_col + 1
+                self.lines[current_row] = (
+                    self.lines[current_row][:current_col]
+                    + "X" * num_moves
+                    + self.lines[current_row][new_position[1] + 1 :]
+                )
+            else:
+                new_position = (-1, -1)
+                self.lines[current_row] = self.lines[current_row][
+                    :current_col
+                ] + "X" * (self.n - current_col)
+            direction = "v"
+        elif direction == "v":
+            try:
+                next_obstruction = (
+                    self.columns[current_col][current_row:].find("#") + current_row
+                )
+            except:
+                next_obstruction = -1
+            if next_obstruction > new_position[0] + 1:
+                new_position = (next_obstruction - 1, current_col)
+                for row in range(current_row, new_position[0] + 1):
+                    self.lines[row] = (
+                        self.lines[row][:current_col]
+                        + "X"
+                        + self.lines[row][current_col + 1 :]
+                    )
+            else:
+                new_position = (-1, -1)
+                for row in range(current_row, self.n):
+                    self.lines[row] = (
+                        self.lines[row][:current_col]
+                        + "X"
+                        + self.lines[row][current_col + 1 :]
+                    )
+
+            direction = "<"
+        else:
+            try:
+                next_obstruction = self.lines[current_row][:current_col].find("#")
+            except:
+                next_obstruction = -1
+            if next_obstruction < new_position[1] - 1 and next_obstruction >= 0:
+                new_position = (current_row, next_obstruction + 1)
+                num_moves = current_col - new_position[1] + 1
+                self.lines[current_row] = (
+                    self.lines[current_row][: new_position[1]]
+                    + "X" * num_moves
+                    + self.lines[current_row][current_col + 1 :]
+                )
+            else:
+                new_position = (-1, -1)
+                self.lines[current_row] = (
+                    "X" * current_col + self.lines[current_row][current_col + 1 :]
+                )
+
+            direction = "^"
+        return new_position, direction
+
     def make_move(self, start, direction, obstructed=False, debug=False):
         current_row, current_col = start
-        if obstructed:
-            self.obstructed_update(current_row, current_col, direction, debug)
-        else:
-            self.lines[current_row] = (
-                self.lines[current_row][:current_col]
-                + "X"
-                + self.lines[current_row][current_col + 1 :]
-            )
-        if direction == "^":
-            if current_row == 0:
-                current_row = -1
-            elif self.check_up(current_row, current_col):
-                direction = ">"
-            else:
-                current_row = current_row - 1
-        elif direction == ">":
-            if current_col == self.m - 1:
-                current_col = self.m
-            elif self.check_right(current_row, current_col):
-                direction = "v"
-            else:
-                current_col += 1
-        elif direction == "v":
-            if current_row == self.n - 1:
-                current_row = self.n
-            elif self.check_down(current_row, current_col):
-                direction = "<"
-            else:
-                current_row += 1
-        else:
-            if current_col == 0:
-                current_col = -1
-            elif self.check_left(current_row, current_col):
-                direction = "^"
-            else:
-                current_col = current_col - 1
+        (current_row, current_col), direction = self.find_next_obstruction(
+            current_row, current_col, direction
+        )
+        # if obstructed:
+        #    self.obstructed_update(current_row, current_col, direction, debug)
+
         return current_row, current_col, direction
 
     def walk(self, obstructed=False, debug=False):
         current_row, current_col = self.start
-        direction = self.direction
-        position_count = 0
+        direction = self.start_direction
         outside = False
         while current_row in range(self.m - 1) and current_col in range(self.n - 1):
             previous_row, previous_col = current_row, current_col
@@ -121,14 +190,12 @@ class LabMap:
             )
             if debug == True:
                 print("\n")
-                print("\n".join(line for line in self.debug_map))
+                print(self)
                 print(current_row, current_col)
 
             if current_row < 0 or current_col < 0:
                 outside = True
                 break
-            elif self.lines[current_row][current_col] != "X":
-                position_count += 1
 
             if obstructed:
                 count = collections.Counter(self.visited[(previous_row, previous_col)])
@@ -138,16 +205,16 @@ class LabMap:
                 else:
                     continue
 
-        if position_count > 0 and not outside:
+        position_count = 0
+        for line in self.lines:
+            position_count += collections.Counter(line)["X"]
+        if not outside:
             position_count += 1
-            if debug == True:
-                print("\n")
-                print("\n".join(line for line in self.debug_map))
         return position_count
 
     def add_obstruction(self, position):
         row, col = position
-        self.lines[row] = self.lines[row][:col] + "O" + self.lines[row][col + 1 :]
+        self.lines[row] = self.lines[row][:col] + "#" + self.lines[row][col + 1 :]
 
     def non_obstructed_spaces(self):
         positions = []
@@ -185,14 +252,15 @@ if __name__ == "__main__":
     answer1 = answer_map.walk()
     print(answer1)
 
-    test_map.reset()
+    # test_map.reset()
 
-    positions = [(6, 3), (7, 6), (7, 7), (8, 1), (8, 3), (9, 7)]
-    assert test_map.test_for_loops(positions) == 6
+    # positions = [(6, 3), (7, 6), (7, 7), (8, 1), (8, 3), (9, 7)]
+    # assert test_map.test_for_loops(positions) == 6
 
     # test_map.reset()
     # test_map.walk()
     # positions = test_map.non_obstructed_spaces()
+    # test_map.reset()
     # assert test_map.test_for_loops(positions) == test_answer2
 
     # answer_positions = answer_map.non_obstructed_spaces()
@@ -200,3 +268,5 @@ if __name__ == "__main__":
     # print(answer2)
 
 # 1794 too low (and slow)
+
+# no longer working for part 1
