91ba9941140ff6345ef225b2f931c7fd29732d56
Kathryn Kleijn <137423893+kathrynpotten@users.noreply.github.com>
2024-12-10 15:28:02 +0000

    7-1 Bridge Repair

--- scripts/07_Bridge_Repair.py
+++ scripts/07_Bridge_Repair.py
@@ -17,7 +17,17 @@ test_data = """190: 10 19
 )
 
 test_answer = 3749
-ops = [sum, math.prod]
+test_answer2 = 11387
+
+
+def concatentaion(value1, value2):
+    return int(str(value1) + str(value2))
+
+
+assert concatentaion(15, 6) == 156
+
+ops_1 = [sum, math.prod, concatentaion]
+ops_2 = [sum, math.prod]
 
 
 def parse_data(input):
@@ -35,19 +45,9 @@ test_data_short = """190: 10 19
 assert parse_data(test_data_short) == {"190": ["10", "19"], "3267": ["81", "40", "27"]}
 
 
-def check_simple(key, value):
-    for op in ops:
-        if op([int(val) for val in value]) == int(key):
-            return int(key)
-    return False
-
-
-assert check_simple("190", ["10", "19"]) == 190
-
-
-def check_permutations(key, value):
+def check_permutations(key, value, ops):
     num_operators = len(value) - 1
-    for perm in itertools.permutations(ops, num_operators):
+    for perm in itertools.product(ops, repeat=num_operators):
         layer = perm[0](([int(value[0]), int(value[1])]))
         for i in range(1, num_operators):
             layer = perm[i]([layer, int(value[i + 1])])
@@ -59,15 +59,23 @@ def check_permutations(key, value):
 
 assert check_permutations("190", ["10", "19"]) == 190
 assert check_permutations("3267", ["81", "40", "27"]) == 3267
+assert check_permutations("292", ["11", "6", "16", "20"]) == 292
 
 
 def find_calibration_possibilities(input):
     total_cal = 0
     for key, value in input.items():
-        simple = check_simple(key, value)
-        if simple:
-            total_cal += 0
-            continue
-        else:
-            pass
+        total_cal += check_permutations(key, value)
     return total_cal
+
+
+test_parsed = parse_data(test_data)
+assert find_calibration_possibilities(test_parsed) == test_answer
+
+if __name__ == "__main__":
+    with open("../input_data/07_Bridge_Repair.txt", "r", encoding="utf-8") as file:
+        input = file.read().strip().split("\n")
+
+    parsed = parse_data(input)
+    answer1 = find_calibration_possibilities(parsed)
+    print(answer1)
