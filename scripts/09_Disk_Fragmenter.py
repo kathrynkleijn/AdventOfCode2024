058e57c8918c495eca4370c98719237e00b87577
Kathryn Kleijn <137423893+kathrynpotten@users.noreply.github.com>
2024-12-11 15:16:58 +0000

    9-2 Disk Fragmenter

--- scripts/09_Disk_Fragmenter.py
+++ scripts/09_Disk_Fragmenter.py
@@ -1,5 +1,7 @@
 # Day 9 - Disk Fragmenter
 
+from collections import OrderedDict
+
 test_data1 = "12345"
 test_answer1 = 60
 
@@ -73,5 +75,79 @@ with open("../input_data/09_Disk_Fragmenter.txt", "r", encoding="utf-8") as file
 
 input = "".join(line for line in input)
 
-answer1 = compute_checksum(input)
-print(answer1)
+# answer1 = compute_checksum(input)
+# print(answer1)
+
+
+# Part 2
+
+
+def parse_data(input):
+    input_list = [char for char in input]
+    files = OrderedDict()
+    free = OrderedDict()
+    index = 0
+    for num, point in enumerate(input_list):
+        if num % 2:
+            free[index] = int(point)
+        else:
+            files[int(num / 2)] = [index, int(point)]
+        index += int(point)
+
+    return files, free
+
+
+assert parse_data(test_data1) == (
+    {0: [0, 1], 1: [3, 3], 2: [10, 5]},
+    {1: 2, 6: 4},
+)
+
+
+def move_files(files, free):
+    for key, value in reversed(files.items()):
+        first_empty_length = list(free.values())[0]
+        print(key, value[1], first_empty_length)
+        if value[1] <= first_empty_length:
+            files[key] = [list(free.keys())[0], value[1]]
+            remaining_free = first_empty_length - value[1]
+            del free[list(free.keys())[0]]
+            if remaining_free:
+                new_index = list(free.keys())[0] + value[1]
+                free[new_index] = remaining_free
+                free = OrderedDict(sorted(free.items()))
+    return files, free
+
+
+def files_repr(files, free):
+    parsed = []
+    for id, value in files.items():
+        parsed.extend([id] * value[1])
+    for index, empty in free.items():
+        for i in range(empty):
+            parsed.insert(index + i, ".")
+    return parsed
+
+
+files1, free1 = parse_data(test_data1)
+assert files_repr(files1, free1) == [
+    0,
+    ".",
+    ".",
+    1,
+    1,
+    1,
+    ".",
+    ".",
+    ".",
+    ".",
+    2,
+    2,
+    2,
+    2,
+    2,
+]
+
+
+files, free = parse_data(test_data2)
+files, free = move_files(files, free)
+print(files_repr(files, free))
