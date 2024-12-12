987fdf359c0ed07f409f5976769e42ece46e1320
Kathryn Kleijn <137423893+kathrynpotten@users.noreply.github.com>
2024-12-12 12:09:09 +0000

    10-1 Hoof it

--- scripts/10_Hoof_It.py
+++ scripts/10_Hoof_It.py
@@ -27,7 +27,7 @@ class HikingMap:
         self.m = len(self.rows)
         self.n = len(self.columns)
         self.size = (self.m, self.n)
-        self.debug = self.rows.copy()
+        self.debug = self.to_rows()
         self.ends = self.possible_ends()
 
     def __str__(self):
@@ -46,30 +46,45 @@ class HikingMap:
             self.columns.append([row[col] for row in self.rows])
         return self.columns
 
-    def make_move(self, current_row, current_col, previous_row, previous_col, height):
-        if (
-            current_col < self.n - 1
-            and self.rows[current_row][current_col + 1] == height + 1
-        ):
+    # def make_move(self, current_row, current_col, previous_row, previous_col, height):
+    #     if (
+    #         current_col < self.n - 1
+    #         and self.rows[current_row][current_col + 1] == height + 1
+    #     ):
+    #         previous_row, previous_col = current_row, current_col
+    #         current_col += 1
+
+    #     elif (
+    #         current_row < self.m - 1
+    #         and self.columns[current_col][current_row + 1] == height + 1
+    #     ):
+    #         previous_row, previous_col = current_row, current_col
+    #         current_row += 1
+    #     elif (
+    #         current_row > 0 and self.columns[current_col][current_row - 1] == height + 1
+    #     ):
+    #         previous_row, previous_col = current_row, current_col
+    #         current_row = current_row - 1
+    #     elif current_col > 0 and self.rows[current_row][current_col - 1] == height + 1:
+    #         previous_row, previous_col = current_row, current_col
+    #         current_col = current_col - 1
+    #     else:
+    #         current_row, current_col = previous_row, previous_col
+    #     return current_row, current_col, previous_row, previous_col
+
+    def make_move(self, current_row, current_col, previous_row, previous_col, move):
+        if move == "right":
             previous_row, previous_col = current_row, current_col
             current_col += 1
 
-        elif (
-            current_row < self.m - 1
-            and self.columns[current_col][current_row + 1] == height + 1
-        ):
+        elif move == "down":
             previous_row, previous_col = current_row, current_col
             current_row += 1
-        elif (
-            current_row > 0 and self.columns[current_col][current_row - 1] == height + 1
-        ):
+        elif move == "up":
             previous_row, previous_col = current_row, current_col
             current_row = current_row - 1
-        elif current_col > 0 and self.rows[current_row][current_col - 1] == height + 1:
-            previous_row, previous_col = current_row, current_col
+        elif move == "left":
             current_col = current_col - 1
-        else:
-            current_row, current_col = previous_row, previous_col
         return current_row, current_col, previous_row, previous_col
 
     def possible_moves(self, current_row, current_col, height):
@@ -94,27 +109,82 @@ class HikingMap:
     def trail(self, trailhead, debug=False):
         current_row, current_col = trailhead
         previous_row, previous_col = trailhead
+        alternatives = []
         height = 0
         trail = True
         while height < 9:
             if debug:
-                print(current_row, current_col, height)
+                # print(current_row, current_col, height)
                 self.debug[current_row][current_col] = "X"
                 debug_repr = ""
                 for line in self.debug:
                     line = "".join(str(char) for char in line)
                     debug_repr += "\n" + line
 
-                print(debug_repr)
-            current_row, current_col, previous_row, previous_col = self.make_move(
-                current_row, current_col, previous_row, previous_col, height
-            )
-            if (previous_row, previous_col) == (current_row, current_col):
-                trail = False
+                # print(debug_repr)
+            moves = self.possible_moves(current_row, current_col, height)
+            if not moves:
+                current_row, current_col = previous_row, previous_col
                 break
+            elif len(moves) == 1:
+                current_row, current_col, previous_row, previous_col = self.make_move(
+                    current_row, current_col, previous_row, previous_col, moves[0]
+                )
+            else:
+                alternatives = self.setup_alternatives(
+                    current_row, current_col, moves[1:]
+                )
+                current_row, current_col, previous_row, previous_col = self.make_move(
+                    current_row, current_col, previous_row, previous_col, moves[0]
+                )
+
             height = self.rows[current_row][current_col]
 
-        return trail
+        if debug:
+            print(debug_repr)
+            self.debug = self.to_rows()
+
+        return trail, alternatives, current_row, current_col
+
+    def setup_alternatives(self, current_row, current_col, moves):
+        alternatives = []
+        for move in moves:
+            new_row, new_col = current_row, current_col
+            if move == "right":
+                new_col = current_col + 1
+            elif move == "down":
+                new_row = current_row + 1
+            elif move == "left":
+                new_col = current_col - 1
+            elif move == "up":
+                new_row = current_row - 1
+            alternatives.append((new_row, new_col))
+        return alternatives
+
+    # def trail(self, trailhead, debug=False):
+    #     current_row, current_col = trailhead
+    #     previous_row, previous_col = trailhead
+    #     height = 0
+    #     trail = True
+    #     while height < 9:
+    #         if debug:
+    #             print(current_row, current_col, height)
+    #             self.debug[current_row][current_col] = "X"
+    #             debug_repr = ""
+    #             for line in self.debug:
+    #                 line = "".join(str(char) for char in line)
+    #                 debug_repr += "\n" + line
+
+    #             print(debug_repr)
+    #         current_row, current_col, previous_row, previous_col = self.make_move(
+    #             current_row, current_col, previous_row, previous_col, height
+    #         )
+    #         if (previous_row, previous_col) == (current_row, current_col):
+    #             trail = False
+    #             break
+    #         height = self.rows[current_row][current_col]
+
+    #     return trail
 
     def possible_trailheads(self):
         trailheads = []
@@ -140,12 +210,42 @@ class HikingMap:
 
     def count_num_trails(self, trailhead, debug=False):
         num_trails = 0
-        # loop over possibilities
-        while ...:
+        trailheads = [trailhead]
+        tops = []
+        while trailheads:
+            trailhead = trailheads[0]
+            top = self.check_top(trailhead)
+            print(f"trailhead = {trailhead}")
+            if top:
+                trail = True
+                current_row, current_col = trailhead
+            else:
+                trail, alternatives, current_row, current_col = self.trail(
+                    trailhead, debug
+                )
+                trailheads.extend(alternatives)
+            if trail:
+                print(f"place = {current_row, current_col}")
+                print(f"tops = {tops}")
+                if (current_row, current_col) not in tops:
+                    tops.append((current_row, current_col))
+                    num_trails += 1
+                else:
+                    break
             if num_trails == len(self.ends):
                 break
+            print(f"tops = {tops}")
+            print(f"num = {num_trails}")
+            trailheads.pop(0)
 
         return num_trails
+
+    def check_top(self, trailhead):
+        row, col = trailhead
+        if self.rows[row][col] == 9:
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
@@ -159,9 +259,15 @@ if __name__ == "__main__":
 
     # test_map1.trail(trailhead, debug=True)
 
+    # print(test_map1.count_num_trails(trailhead))
+
     test_map2 = HikingMap(test_data2)
 
     assert len(test_map2.possible_trailheads()) == 9
     assert len(test_map2.possible_ends()) == 7
 
     # test_map2.trail((0, 2), debug=True)
+
+    print(test_map2.count_num_trails((0, 2), debug=True))
+
+    # works for some, but how do we go back and re-check the start?
