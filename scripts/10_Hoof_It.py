d3e3b0e51f86482e313eb74b1a46c0f97239fec0
Kathryn Kleijn <137423893+kathrynpotten@users.noreply.github.com>
2024-12-12 14:14:27 +0000

    10-1 Hoof It

--- scripts/10_Hoof_It.py
+++ scripts/10_Hoof_It.py
@@ -1,5 +1,7 @@
 # Day 10 - Hoof It
 
+from collections import defaultdict, OrderedDict
+
 test_data1 = """0123
 1234
 8765
@@ -15,7 +17,31 @@ test_data2 = """89010123
 10456732"""
 
 test_answer1 = 1
-test_answer2 = 9
+test_answer2 = 36
+
+test_data3 = """...0...
+...1...
+...2...
+6543456
+7.....7
+8.....8
+9.....9"""
+
+test_data4 = """..90..9
+...1.98
+...2..7
+6543456
+765.987
+876....
+987...."""
+
+test_data5 = """10..9..
+2...8..
+3...7..
+4567654
+...8..3
+...9..2
+.....01"""
 
 
 class HikingMap:
@@ -34,11 +60,16 @@ class HikingMap:
         return "\n".join(line for line in self.rows)
 
     def to_rows(self):
-        self.rows = []
+        rows = []
         lines = self.map.split("\n")
         for line in lines:
-            self.rows.append([int(char) for char in line])
-        return self.rows
+            row = [char for char in line]
+            for i in range(len(row)):
+                if row[i] != ".":
+                    row[i] = int(row[i])
+
+            rows.append(row)
+        return rows
 
     def to_columns(self):
         columns = []
@@ -63,42 +94,53 @@ class HikingMap:
 
     def possible_moves(self, current_row, current_col, height):
         moves = []
+
         if (
             current_col < self.n - 1
+            and self.rows[current_row][current_col + 1] != "."
             and self.rows[current_row][current_col + 1] == height + 1
         ):
             moves.append("right")
-
         if (
             current_row < self.m - 1
+            and self.columns[current_col][current_row + 1] != "."
             and self.columns[current_col][current_row + 1] == height + 1
         ):
             moves.append("down")
-        if current_row > 0 and self.columns[current_col][current_row - 1] == height + 1:
+        if (
+            current_row > 0
+            and self.columns[current_col][current_row - 1] != "."
+            and self.columns[current_col][current_row - 1] == height + 1
+        ):
             moves.append("up")
-        if current_col > 0 and self.rows[current_row][current_col - 1] == height + 1:
+        if (
+            current_col > 0
+            and self.rows[current_row][current_col - 1]
+            and self.rows[current_row][current_col - 1] == height + 1
+        ):
             moves.append("left")
         return moves
 
-    def trail(self, trailhead, debug=False):
+    def trail(self, trailhead, height, debug=False):
         current_row, current_col = trailhead
         previous_row, previous_col = trailhead
-        alternatives = []
-        height = 0
+        alternatives = defaultdict()
+        height = height
         trail = True
         while height < 9:
             if debug:
-                # print(current_row, current_col, height)
+                print(current_row, current_col, height)
                 self.debug[current_row][current_col] = "X"
                 debug_repr = ""
                 for line in self.debug:
                     line = "".join(str(char) for char in line)
                     debug_repr += "\n" + line
 
-                # print(debug_repr)
+                print(debug_repr)
             moves = self.possible_moves(current_row, current_col, height)
             if not moves:
                 current_row, current_col = previous_row, previous_col
+                trail = False
                 break
             elif len(moves) == 1:
                 current_row, current_col, previous_row, previous_col = self.make_move(
@@ -109,17 +151,16 @@ class HikingMap:
                 current_row, current_col, previous_row, previous_col = self.make_move(
                     current_row, current_col, previous_row, previous_col, moves[0]
                 )
-                alternatives.extend(alts)
+                for alt in alts:
+                    alternatives[alt] = height + 1
 
             height = self.rows[current_row][current_col]
-            # print(alternatives)
 
-        print(self.rows)
         if debug:
             print(debug_repr)
             self.debug = self.to_rows()
 
-        return trail, alternatives, current_row, current_col
+        return trail, alternatives, current_row, current_col, height
 
     def setup_alternatives(self, current_row, current_col, moves):
         alternatives = []
@@ -160,32 +201,39 @@ class HikingMap:
 
     def count_num_trails(self, trailhead, debug=False):
         num_trails = 0
-        trailheads = [trailhead]
+        trailheads = OrderedDict()
+        trailheads[trailhead] = 0
+        checked = []
         tops = []
         while trailheads:
-            trailhead = trailheads[0]
+            trailhead = list(trailheads.keys())[0]
+            height = trailheads[trailhead]
+            if trailhead in checked:
+                trailheads.pop(trailhead, None)
+                continue
             top = self.check_top(trailhead)
-            # print(f"trailhead = {trailhead}")
             if top:
                 trail = True
                 current_row, current_col = trailhead
             else:
-                trail, alternatives, current_row, current_col = self.trail(
-                    trailhead, debug
+                trail, alternatives, current_row, current_col, height = self.trail(
+                    trailhead, height, debug
                 )
-                trailheads.extend(alternatives)
+                for alternative, height in alternatives.items():
+                    trailheads[alternative] = height
             if trail:
-                # print(f"place = {current_row, current_col}")
-                # print(f"tops = {tops}")
                 if (current_row, current_col) not in tops:
                     tops.append((current_row, current_col))
                     num_trails += 1
             if num_trails == len(self.ends):
                 break
-            # print(f"tops = {tops}")
-            # print(f"num = {num_trails}")
-            trailheads.pop(0)
+            try:
+                trailheads.pop(trailhead)
+            except:
+                break
 
+            checked.append(trailhead)
+            checked = list(set(checked))
         return num_trails
 
     def check_top(self, trailhead):
@@ -194,6 +242,13 @@ class HikingMap:
             return True
         else:
             return False
+
+    def count_all_trails(self, debug=False):
+        total = 0
+        trailheads = self.possible_trailheads()
+        for trailhead in trailheads:
+            total += self.count_num_trails(trailhead, debug)
+        return total
 
 
 if __name__ == "__main__":
@@ -207,7 +262,8 @@ if __name__ == "__main__":
 
     # test_map1.trail(trailhead, debug=True)
 
-    # assert test_map1.count_num_trails(trailhead) == 1
+    assert test_map1.count_num_trails(trailhead) == 1
+    assert test_map1.count_all_trails() == test_answer1
 
     test_map2 = HikingMap(test_data2)
 
@@ -216,6 +272,18 @@ if __name__ == "__main__":
 
     # test_map2.trail((0, 2), debug=True)
 
-    assert test_map2.count_num_trails((0, 2), debug=True) == 5
+    assert test_map2.count_all_trails() == test_answer2
 
-    # something off with the debug - with debug= False, does not break
+    test_map3 = HikingMap(test_data3)
+    assert test_map3.count_all_trails() == 2
+    test_map4 = HikingMap(test_data4)
+    assert test_map4.count_all_trails() == 4
+    test_map5 = HikingMap(test_data5)
+    assert test_map5.count_all_trails() == 3
+
+    with open("../input_data/10_Hoof_It.txt", "r", encoding="utf-8") as file:
+        input = file.read().strip()
+
+    answer_map = HikingMap(input)
+    answer1 = answer_map.count_all_trails()
+    print(answer1)
