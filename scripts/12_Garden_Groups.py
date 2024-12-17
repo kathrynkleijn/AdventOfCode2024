af3a9afb5779679aeba7fe51f9371cdda47e48f1
Kathryn Kleijn <137423893+kathrynpotten@users.noreply.github.com>
2024-12-17 16:04:33 +0000

    12-1 Garden Groups

--- scripts/12_Garden_Groups.py
+++ scripts/12_Garden_Groups.py
@@ -37,66 +37,69 @@ class Garden:
         return list(set(filtered))
 
     def find_region(self, plant_type):
-        plant_region = []
-        for row in self.rows:
-            new_row = [plant if plant == plant_type else "." for plant in row]
-            plant_region.append(new_row)
-        return plant_region
+        perimeter = 0
+        area = 0
+        for i, row in enumerate(self.rows):
+            for j, plant in enumerate(row):
+                if plant == plant_type:
+                    area += 1
+                    perimeter += self.perimeter_of_plant((i, j), plant_type)
+        return area, perimeter
 
-    def perimeter_of_plant(self, plant_region, plant_position):
+    def perimeter_of_plant(self, plant_position, plant_type):
         perimeter = 0
         i, j = plant_position
 
         if i == 0:
             perimeter += 1
-        elif plant_region[i - 1][j] == ".":
+        elif self.rows[i - 1][j] != plant_type:
             perimeter += 1
 
         if i == self.m - 1:
             perimeter += 1
-        elif plant_region[i + 1][j] == ".":
+        elif self.rows[i + 1][j] != plant_type:
             perimeter += 1
 
         if j == 0:
             perimeter += 1
-        elif plant_region[i][j - 1] == ".":
+        elif self.rows[i][j - 1] != plant_type:
             perimeter += 1
 
         if j == self.n - 1:
             perimeter += 1
-        elif plant_region[i][j + 1] == ".":
+        elif self.rows[i][j + 1] != plant_type:
             perimeter += 1
 
         return perimeter
 
-    def perimeter_of_region(self, plant_region):
-        plant_positions = []
-        for i, row in enumerate(plant_region):
-            for j, plant in enumerate(row):
-                if plant != ".":
-                    plant_positions.append((i, j))
-        perimeter = 0
-        for position in plant_positions:
-            perimeter += self.perimeter_of_plant(plant_region, position)
+    # def perimeter_of_region(self, plant_region):
+    #     plant_positions = []
+    #     for i, row in enumerate(plant_region):
+    #         for j, plant in enumerate(row):
+    #             if plant != ".":
+    #                 plant_positions.append((i, j))
+    #     perimeter = 0
+    #     for position in plant_positions:
+    #         perimeter += self.perimeter_of_plant(plant_region, position)
 
-        return perimeter
+    #     return perimeter
 
-    def area_of_region(self, plant_region):
-        area = 0
-        for row in plant_region:
-            for plant in row:
-                if plant != ".":
-                    area += 1
-        return area
+    # def area_of_region(self, plant_region):
+    #     area = 0
+    #     for row in plant_region:
+    #         for plant in row:
+    #             if plant != ".":
+    #                 area += 1
+    #     return area
 
-    def price_of_region(self, plant_region):
-        area = self.area_of_region(plant_region)
-        perimeter = self.perimeter_of_region(plant_region)
+    def price_of_region(self, area, perimeter):
+        # area = self.area_of_region(plant_region)
+        # perimeter = self.perimeter_of_region(plant_region)
         return area * perimeter
 
 
 if __name__ == "__main__":
 
     test_garden = Garden(test_data)
-    plant_region = test_garden.find_region("A")
-    assert test_garden.price_of_region(plant_region) == 40
+    area, perimeter = test_garden.find_region("A")
+    assert test_garden.price_of_region(area, perimeter) == 40
