eaa4154162ab5df3b298d72a8d3ea6c47b2c9082
Kathryn Kleijn <137423893+kathrynpotten@users.noreply.github.com>
2024-12-18 09:50:59 +0000

    12-2 Garden Groups

--- scripts/12_Garden_Groups.py
+++ scripts/12_Garden_Groups.py
@@ -2,6 +2,7 @@
 
 import re
 from random import choice
+from collections import defaultdict
 
 test_data = """AAAA
 BBCD
@@ -9,6 +10,7 @@ BBCC
 EEEC"""
 
 test_answer = 140
+test_answer_2 = 80
 
 test_data2 = """OOOOO
 OXOXO
@@ -30,6 +32,24 @@ MIIISIJEEE
 MMMISSJEEE"""
 
 test_answer3 = 1930
+test_answer3_2 = 1206
+
+test_data4 = """EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE"""
+
+test_answer4 = 236
+
+test_data5 = """AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA"""
+
+test_answer5 = 368
 
 
 class Garden:
@@ -69,7 +89,6 @@ class Garden:
 
         return plant_region
 
-    # make this recursive
     def test_if_connected(self, plant_region, regions=[]):
 
         def neighbours(plant):
@@ -94,54 +113,86 @@ class Garden:
         return regions
 
     def perimeter_of_plant(self, plant_position, plant_type):
+        region_edge = []
         perimeter = 0
         i, j = plant_position
 
         if i == 0:
             perimeter += 1
+            region_edge.append(("row", -1.25, j))
         elif self.rows[i - 1][j] != plant_type:
             perimeter += 1
+            region_edge.append(("row", (i - 1) - 0.25, j))
 
         if i == self.m - 1:
             perimeter += 1
+            region_edge.append(("row", self.m + 0.25, j))
         elif self.rows[i + 1][j] != plant_type:
             perimeter += 1
+            region_edge.append(("row", (i + 1) + 0.25, j))
 
         if j == 0:
             perimeter += 1
+            region_edge.append(("col", -1.25, i))
         elif self.rows[i][j - 1] != plant_type:
             perimeter += 1
+            region_edge.append(("col", (j - 1) - 0.25, i))
 
         if j == self.n - 1:
             perimeter += 1
+            region_edge.append(("col", self.n + 0.25, i))
         elif self.rows[i][j + 1] != plant_type:
             perimeter += 1
+            region_edge.append(("col", (j + 1) + 0.25, i))
 
-        return perimeter
+        return perimeter, region_edge
 
     def perimeter_of_region(self, plant_region, plant_type):
+        region_edges = defaultdict(list)
         perimeter = 0
         for position in plant_region:
-            perimeter += self.perimeter_of_plant(position, plant_type)
+            perim, region_edge = self.perimeter_of_plant(position, plant_type)
+            perimeter += perim
+            for edge in region_edge:
+                edge_type, id1, id2 = edge
+                region_edges[(edge_type, id1)].append(id2)
 
-        return perimeter
+        return perimeter, region_edges
 
     def area_of_region(self, plant_region):
         return len(plant_region)
 
+    def edges_of_region(self, region_edges):
+        num_edges = 0
+        for edge_coords in region_edges.values():
+            edge_coords = sorted(edge_coords)
+            for i, coord in enumerate(edge_coords):
+                if coord != edge_coords[i - 1] + 1:
+                    num_edges += 1
+        return num_edges
+
     def price_of_region(self, plant_region, plant_type):
         area = self.area_of_region(plant_region)
-        perimeter = self.perimeter_of_region(plant_region, plant_type)
+        perimeter, region_edges = self.perimeter_of_region(plant_region, plant_type)
         return area * perimeter
 
-    def price_of_garden(self):
+    def discounted_price_of_region(self, plant_region, plant_type):
+        area = self.area_of_region(plant_region)
+        perimeter, region_edges = self.perimeter_of_region(plant_region, plant_type)
+        num_edges = self.edges_of_region(region_edges)
+        return area * num_edges
+
+    def price_of_garden(self, discount=False):
         price = 0
         for plant in self.plants:
             regions = []
             plant_region = self.find_region(plant)
             all_regions = self.test_if_connected(plant_region, regions)
             for region in all_regions:
-                price += self.price_of_region(region, plant)
+                if discount:
+                    price += self.discounted_price_of_region(region, plant)
+                else:
+                    price += self.price_of_region(region, plant)
 
         return price
 
@@ -170,3 +221,18 @@ if __name__ == "__main__":
     answer_garden = Garden(input)
     answer1 = answer_garden.price_of_garden()
     print(answer1)
+
+    assert test_garden.price_of_garden(discount=True) == test_answer_2
+
+    assert test_garden3.price_of_garden(discount=True) == test_answer3_2
+
+    test_garden4 = Garden(test_data4)
+
+    assert test_garden4.price_of_garden(discount=True) == test_answer4
+
+    test_garden5 = Garden(test_data5)
+
+    assert test_garden5.price_of_garden(discount=True) == test_answer5
+
+    answer2 = answer_garden.price_of_garden(discount=True)
+    print(answer2)
